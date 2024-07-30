721671a0411de716ddc50e038937d80e26780d80
Quang Phan <vnphanquang@gmail.com>
2024-07-30 22:51:31 +0700

    feat: simplify satori run - only render text and inject to pre-built svg template

--- src/index.js
+++ src/index.js
@@ -1,46 +1,78 @@
 import { Hono } from 'hono';
 import satori from 'satori';
-import { html as toReactElement } from 'satori-html';
+import { html as to_react_element } from 'satori-html';
 
-import twkEverettFontData from './fonts/TWKEverett-Regular.otf';
-import { initResgWasm } from './resvg.js';
-import greenTemplate from './templates/green.template.html';
+import twk_everett_font_data from './fonts/TWKEverett-Regular.otf';
+import template_blank from './templates/blank.template.svg';
+import template_green from './templates/green.template.svg';
+import template_text from './templates/text.template.html';
 
 const app = new Hono();
 
 const HEIGHT = 95;
 const WIDTH = 200;
 
-app.get('/', async (c) => {
-	const template = greenTemplate
-		.replace('{{url}}', 'www.yourdomain.xyz')
-		.replace('{{provider}}', 'Acme Hosting');
-	const html = toReactElement(template);
-	const svg = await satori(html, {
-		fonts: [
-			{
-				name: 'TWK Everett',
-				data: twkEverettFontData,
-				style: 'normal',
-			},
-		],
-		height: HEIGHT,
-		width: WIDTH,
-	});
+app.get('/:hostname', async (c) => {
+	const hostname = c.req.param('hostname');
 
-	const Resvg = await initResgWasm();
-	const png = new Resvg(svg, {
-		fitTo: {
-			mode: 'height',
-			value: WIDTH * 2,
-		},
-	})
-		.render()
-		.asPng();
+	let svg = template_blank;
+	let ttl = 86400; // 1 day
+	try {
+		const greencheck = await (
+			await fetch(
+				`https://api.thegreenwebfoundation.org/api/v3/greencheck/${encodeURIComponent(hostname)}`,
+			)
+		).json();
 
-	c.header('Content-Type', 'image/png');
-	c.header('Cache-Control', 'public, immutable, no-transform, max-age=604800');
-	return c.body(png);
+		if (greencheck.green) {
+			const { hosted_by } = greencheck;
+
+			const template = template_text
+				.replace('{{url}}', hostname.slice(0, 30))
+				.replace('{{provider}}', hosted_by);
+			const html = to_react_element(template);
+			const text_svg = await satori(html, {
+				fonts: [
+					{
+						name: 'TWK Everett',
+						data: twk_everett_font_data,
+						style: 'normal',
+					},
+				],
+				height: HEIGHT,
+				width: WIDTH,
+			});
+			const paths = Array.from(text_svg.match(/<path[^>]*\/>/g)).join('');
+			svg = template_green.replace('{{text}}', paths);
+			ttl = 604800; // 1 week
+
+			/**
+			 * ---NOTICE---
+			 * It is possible to convert svg to png using resvg (web-assembly),
+			 * using the code below.
+			 * However, resvg is quite heavy (causing slow cold start) and will increase
+			 * the size of the uploaded Cloudflare worker to more than 1MB,
+			 * which is the limit for free plan.
+			 */
+
+			// const { initResgWasm } = await import('./resvg.js');
+			// const Resvg = await initResgWasm();
+			// const png = new Resvg(svg, {
+			// 	fitTo: {
+			// 		mode: 'height',
+			// 		value: WIDTH * 2,
+			// 	},
+			// })
+			// 	.render()
+			// 	.asPng();
+		}
+	} catch (err) {
+		console.error(err);
+	}
+
+	c.header('Content-Type', 'image/svg+xml');
+	c.header('Cache-Control', `public, immutable, no-transform, max-age=${ttl}`);
+	return c.body(svg);
 });
 
 export default app;
